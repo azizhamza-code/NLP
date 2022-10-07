079b5b16dfb89820d49317de4b67f2c017c77025
AZIZ Hamza <Haziz2170@gmail.com>
2022-10-07 06:35:39 -0700

    debugging and orginizing code

--- setup_resources.py
+++ setup_resources.py
@@ -3,56 +3,54 @@ import os
 import requests
 from tqdm import tqdm
 
-
-
-
 URL_REPO = "https://github.com/azizhamza-code/NLP/"
 
 
+def download_file(url, file_path):
 
-def download_file(url , file_path):
+    r = requests.get(url, stream=True)
+    total_size = int(r.headers.get('content-length'))
 
-   r = requests.get(url,stream=True) 
-   total_size = int(r.headers.get('content-length'))
+    try:
+        with open(file_path, 'wb', buffering=16*1024*1024) as f:
+            bar = tqdm(total=total_size, unit='B', unit_scale=True)
+            bar.set_description(os.path.split(file_path)[-1])
+            for chunk in r.iter_content(32*1024):
+                f.write(chunk)
+                bar.update(len(chunk))
+            bar.close()
+    except Exception:
+        print("download failed")
 
-   try :
-      with open(file_path , 'wb' , buffering= 16*1024*1024) as f:
-         bar = tqdm(total=total_size,unit='B',unit_scale=True)
-         bar.set_description(os.path.split(file_path)[-1])
-         for chunk in r.iter_content(32*1024):
-            f.write(chunk)
-            bar.update(len(chunk))
-         bar.close()
-   except Exception:
-      print("download failed")
-
-   finally : 
-      if os.path.getsize(file_path) != total_size:
-         os.remove(file_path)
-         print("Removed incomplete download")
-
-def download_from_github(version , fn ,target_dir , force= False ):
+    finally:
+        if os.path.getsize(file_path) != total_size:
+            os.remove(file_path)
+            print("Removed incomplete download")
 
 
-   url = URL_REPO + f"releases/download/{version}/{fn}"
-   file_path = os.path.join(target_dir, fn)
-   if os.path.exists(file_path) and not force: 
-      print(f"File{file_path} is already download")
-      return 
+def download_from_github(version, fn, target_dir, force=False):
 
-   download_file(url , file_path)
+    url = URL_REPO + f"releases/download/{version}/{fn}"
+    file_path = os.path.join(target_dir, fn)
+    if os.path.exists(file_path) and not force:
+        print(f"File{file_path} is already download")
+        return
 
-
-def download_iter(target_dir, fns , version , force = False):
-   os.makedirs(target_dir , exist_ok = True)
-   for fn in fns :
-      download_from_github(version, fn, target_dir, force)
+    download_file(url, file_path)
 
 
-def data_bag_of_words(force =False):
-   download_iter(target_dir= "data",
-   fns = ["train.tsv","validation.tsv", "test.tsv","text_prepare_tests.tsv"],
-   version = "datav1",
-   force = False
+def download_iter(target_dir, fns, version, force=False):
+    os.makedirs(target_dir, exist_ok=True)
+    for fn in fns:
+        download_from_github(version, fn, target_dir, force)
 
-   )
+
+def data_bag_of_words(target_dir="data", force=False):
+
+    download_iter(target_dir=target_dir,
+                  fns=["train.tsv", "validation.tsv",
+                       "test.tsv", "text_prepare_tests.tsv"],
+                  version="datav1",
+                  force=False
+
+                  )
---
+++ text_as_bagofword/utils.py
@@ -0,0 +1,28 @@
+
+import sys
+import os
+sys.path.append(os.path.join("..","NLP"))
+
+import pandas as pd
+from setup_resources import data_bag_of_words
+
+from pathlib import Path
+
+
+def import_data():
+
+    path_file = Path(__file__).parent.parent
+    data_path = os.path.join(path_file, "data")
+
+    # setup data if needed
+
+    train_data = os.path.join(data_path, "train.tsv")
+    val_data = os.path.join(data_path, "validation.tsv")
+    test_data = os.path.join(data_path, "test.tsv")
+
+    train = pd.read_csv(train_data, sep='\t')
+    data_bag_of_words(target_dir=data_path)
+    test = pd.read_csv(test_data, sep='\t')
+    val = pd.read_csv(val_data, sep='\t')
+
+    return train, val, test
